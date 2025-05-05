a5831ddd149af5e70ab6c0cf9e41211d437f990c
Grunde Thorheim <grunde.thorheim@arbeidstilsynet.no>
2025-05-05 16:10:58 +0200

    chore(theme): init brands

---
+++ packages/theme/brand/colors.d.ts
@@ -0,0 +1,10 @@
+import type {} from '@digdir/designsystemet-react/colors';
+
+declare module '@digdir/designsystemet-react/colors' {
+  export interface MainAndSupportColors {
+    primary: never;
+    brand-1: never;
+    brand-2: never;
+    brand-3: never;
+  }
+}
