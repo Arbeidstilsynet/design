3e21dfba9f26917147ee4e3faf0cb323e3b7fedd
Grunde Thorheim <grunde.thorheim@arbeidstilsynet.no>
2025-05-06 10:06:23 +0200

    refactor: remove unused TempExample2 component and update exports

--- packages/react/src/components/index.ts
+++ packages/react/src/components/index.ts
@@ -1,2 +1,1 @@
 export * from "./temp-example";
-export * from "./temp-example-2";
--- packages/react/src/components/temp-example-2/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Button, Tooltip } from "@digdir/designsystemet-react";
-
-export function TempExample2() {
-  return (
-    <Tooltip content="Tooltip content 2">
-      <Button>Hover me 2</Button>
-    </Tooltip>
-  );
-}
