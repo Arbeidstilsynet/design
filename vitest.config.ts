7c434f6cc5644cd3febc7189ee8eeb26c9652f52
vaernion <55745090+vaernion@users.noreply.github.com>
2025-05-09 16:31:36 +0200

    chore: add vitest coverage (#29)
    * feat(dependencies): add @vitest/coverage-v8 to enhance test coverage reporting
    
    * WIP
    
    * feat(ci): enhance CI workflow with coverage reporting and unit test publishing
    
    * feat(tests): update reporters to include junit for better test reporting
    
    * feat(tests): remove redundant test coverage scripts from package.json
    
    * feat(ci): add permissions for checks in CI workflow
    
    * feat(ci): add missing permissions for contents in CI workflow
    
    * fix(ci): ensure typecheck stops the workflow if errors are present
    
    * feat(ci): add pull request permissions to CI workflow
    
    * fix(ci): remove unnecessary exit command from typecheck step
    
    * fix(ci): add comments regarding known issues with status checks in report coverage step
    
    * fix(ci): enable comments on unit test report publication
    
    * fix(ci): remove unnecessary check_name field from unit test report configuration
    
    * fix(ci): update test report publication step and remove known issue comments
    
    * fix(ci): add comments regarding known issues with report coverage step

---
+++ vitest.config.ts
@@ -0,0 +1,27 @@
+import { coverageConfigDefaults, defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    workspace: ["packages/*"],
+    reporters: ["default", "junit"],
+    outputFile: {
+      junit: "test-report.xml",
+    },
+    coverage: {
+      enabled: true,
+      reporter: ["html", "json-summary", "json", "lcov"],
+      reportOnFailure: true,
+      exclude: [
+        ...coverageConfigDefaults.exclude,
+        "**/*config.{mjs,ts,js}",
+        "**/.storybook/**",
+        "**/*.stories.tsx",
+      ],
+    },
+    css: {
+      modules: {
+        classNameStrategy: "non-scoped",
+      },
+    },
+  },
+});
