07f0766e82744068a07f941b2c09a08e56a343a1
Grunde Thorheim <grunde.thorheim@arbeidstilsynet.no>
2025-05-14 16:01:55 +0200

    feat: enhance TempExample component with customizable heading and button variant props

--- packages/react/src/components/temp-example/index.tsx
+++ packages/react/src/components/temp-example/index.tsx
@@ -1,20 +1,27 @@
 import { Button, Card, Details, Heading } from "@digdir/designsystemet-react";
+import { ComponentProps } from "react";
 
 interface TempExampleProps {
+  heading?: string;
   onClick: () => void | Promise<void>;
+  buttonVariant?: ComponentProps<typeof Button>["variant"];
 }
 
-export function TempExample({ onClick }: Readonly<TempExampleProps>) {
+export function TempExample({
+  onClick,
+  heading = "Tittel",
+  buttonVariant = "primary",
+}: Readonly<TempExampleProps>) {
   return (
     <Card>
       <Card.Block>
-        <Heading>Tittel</Heading>
+        <Heading>{heading}</Heading>
       </Card.Block>
       <Card.Block>
         <Details>
           <Details.Summary>Oppsummering</Details.Summary>
           <Details.Content>
-            <Button variant="primary" onClick={onClick}>
+            <Button variant={buttonVariant} onClick={onClick}>
               Klikk meg
             </Button>
           </Details.Content>
