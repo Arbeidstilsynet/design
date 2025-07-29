0cb3b781a0b23b66809674bb302027e1c41eb7ed
Gjøran <gjoran.moen@arbeidstilsynet.no>
2025-07-29 09:51:55 +0200

    Fixed "ReferenceError: __dirname is not defined" error on storybook build/dev

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -1,5 +1,9 @@
 import type { StorybookConfig } from "@storybook/react-vite";
-import { resolve } from "node:path";
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const config: StorybookConfig = {
   stories: [
