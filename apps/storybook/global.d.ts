6468fc0fc3931e5903e74e5ebb9532a66693953c
Grunde Thorheim <grunde.thorheim@arbeidstilsynet.no>
2025-08-05 15:04:37 +0200

    fix storybook tsconfig

--- apps/storybook/global.d.ts
+++ apps/storybook/global.d.ts
@@ -7,3 +7,9 @@ declare module "*.jpg" {
   const src: string;
   export default src;
 }
+
+// Generic fallback for any CSS files
+declare module "*.css" {
+  const content: void;
+  export default content;
+}
