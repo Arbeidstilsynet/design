1ebb6468d46144a599978fdb831d8bfb67db45f7
Roberth Tollefsen <roberth.tollefsen@arbeidstilsynet.no>
2025-08-12 09:01:14 +0200

    add test

--- packages/react/src/components/Header/header.test.tsx
+++ packages/react/src/components/Header/header.test.tsx
@@ -27,4 +27,27 @@ describe("Header", () => {
     expect(screen.getByText("Home")).toBeInTheDocument();
   });
 
+  test('renders without children', () => {
+    render(<Header {...defaultProps} />);
+    expect(screen.queryByText("Miljøbanner")).not.toBeInTheDocument();
+    expect(screen.queryByText("Fagsystem")).not.toBeInTheDocument();
+    expect(screen.queryByText("Søk")).not.toBeInTheDocument();
+    expect(screen.queryByText("Bruker Brukersen")).not.toBeInTheDocument();
+  });
+
+  test('applies custom arguments for all children', () => {
+
+    render(
+      <Header {...defaultProps}>
+        <Header.Banner text="båt" />
+        <Header.Title fagsystemNavn="bil" brukernavn="Jsn for" />
+        <Header.Search />
+        <Header.Links links={[{ href: "#", text: "Custom Link" }]} />
+      </Header>
+    );
+    expect(screen.getByText("båt")).toBeInTheDocument();
+    expect(screen.getByText("bil")).toBeInTheDocument();
+    expect(screen.getByText("Jsn for")).toBeInTheDocument();
+    expect(screen.getByText("Custom Link")).toBeInTheDocument();
+  });
 })
