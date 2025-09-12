952726b2a8e8678f48d52276a0d00ba45eda7dba
Grunde Thorheim <grunde.thorheim@arbeidstilsynet.no>
2025-09-12 10:26:25 +0200

    chore(react): add exports

--- packages/react/src/components/index.ts
+++ packages/react/src/components/index.ts
@@ -1,2 +1,5 @@
 export * from "./filepicker";
 export * from "./header";
+export { LogoBlack } from "./logo/logoblack";
+export { LogoGreen } from "./logo/logogreen";
+export { LogoWhite } from "./logo/logowhite";
