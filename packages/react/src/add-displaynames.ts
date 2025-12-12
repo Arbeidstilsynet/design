367890cb5e98fa30cf2cb8fee5da67750b6f676a
Steffen Grønmo <steffen.gronmo@arbeidstilsynet.no>
2025-12-12 11:46:41 +0100

    fix: Added displayname to externally imported components, to improve the readability of our code docs

---
+++ packages/react/src/add-displaynames.ts
@@ -0,0 +1,18 @@
+// Hack to make sure all components are displayed correctly in code snippets in Storybook Docs
+import * as designsystemet from "@digdir/designsystemet-react";
+import * as akselIcons from "@navikt/aksel-icons";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const addDisplaynames = (pkg: any) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  Object.keys(pkg).forEach((key) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    if ('render' in pkg[key]) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      pkg[key].displayName = key;
+    }
+  });
+};
+
+addDisplaynames(designsystemet);
+addDisplaynames(akselIcons);
--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,3 +1,4 @@
+import "./add-displaynames";
 export * from "./components";
 export * from "./digdir";
 export type { Color, Size } from "./types";
