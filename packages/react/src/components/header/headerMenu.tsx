02c6c74df3676e6790f30597b24d20a39d37aa50
Steffen Grønmo <steffen.gronmo@arbeidstilsynet.no>
2026-02-06 12:46:40 +0100

    fix: data-size is now set to sm for mobile view only

--- packages/react/src/components/header/headerMenu.tsx
+++ packages/react/src/components/header/headerMenu.tsx
@@ -15,7 +15,7 @@ import {
   type ReactNode,
 } from "react";
 import { Button, Divider, Dropdown } from "../../digdir";
-import type { DefaultProps } from "../../types";
+import type { DefaultProps, Size } from "../../types";
 import { useMediaQuery } from "../hooks/useMediaQuery";
 import { HeaderContext } from "./headerContext";
 
@@ -119,6 +119,7 @@ export function HeaderMenu({
       <Dropdown.TriggerContext>
         {/* Have to use a shared Dropdown.Trigger for desktop/mobile or the positioning in Popover becomes wrong */}
         <Dropdown.Trigger
+          data-size={(isMobile ? "sm" : undefined) as Size}
           variant={isMobile ? "primary" : "tertiary"}
           className="at-header__dropdown-trigger"
         >
