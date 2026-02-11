7851d8defa8540e8be2486ba0b51187af9b7d293
steffen-gronmo <steffen.gronmo@arbeidstilsynet.no>
2026-02-11 12:54:22 +0100

    fix(docs): Changes to color docs (#628)
    Changes:
    - Updated the color docs (Groups special groups together with some
    description + data-color selector)
    - Added whitespace to brand .css file to better see what is going on
    - Removed weird logic that caused the selected data color to reset once
    `data-color-scheme` is defined under an ancestor with `data-color`
    attribute (Probably only ever happened in storybook, but weird logic)
    
    TODO:
    - Verify that the `data-color-scheme` is in deed a bug
    
    ---------
    
    Co-authored-by: Grunde Thorheim <grunde.thorheim@arbeidstilsynet.no>

---
+++ apps/storybook/stories/Colors/ColorPalette.tsx
@@ -0,0 +1,55 @@
+import { Alert } from "@arbeidstilsynet/design-react";
+import React from "react";
+import { toast } from "react-toastify";
+import classes from "./ColorPalette.module.css";
+
+type ColorPaletteV2Props = {
+  palette: Record<string, Record<string, string>>;
+};
+
+export const ColorPalette = ({ palette }: ColorPaletteV2Props) => {
+  return (
+    <div className={classes.palette}>
+      {Object.entries(palette).map(([colorgroup, colors]) => (
+        <React.Fragment key={colorgroup}>
+          <div className={classes.groupName}>{colorgroup}</div>
+          <div>
+            <div className={classes.colorRow}>
+              {Object.entries(colors).map(([key, value]) => (
+                <button
+                  key={key}
+                  className={classes.colorItem}
+                  style={{ background: value }}
+                  onClick={() =>
+                    copyToClipboard(
+                      `var(--ds-color-${colorgroup.toLowerCase()}-${key})`,
+                    )
+                  }
+                />
+              ))}
+            </div>
+            <div className={classes.bottomTextRow}>
+              {Object.entries(colors).map(([key, value]) => (
+                <div key={key} className={classes.bottomTextItem}>
+                  {key}
+                  <br />
+                  {value.toUpperCase()}
+                </div>
+              ))}
+            </div>
+          </div>
+        </React.Fragment>
+      ))}
+    </div>
+  );
+};
+
+const copyToClipboard = (text: string) => {
+  navigator.clipboard.writeText(text);
+  toast(
+    <Alert data-size="sm" data-color="success" style={{ width: "100%" }}>
+      Copied CSS variable to clipboard
+    </Alert>,
+    { autoClose: 1500, className: classes.toast },
+  );
+};
--- apps/storybook/stories/Colors/ColorsSwatches.tsx
+++ apps/storybook/stories/Colors/ColorsSwatches.tsx
@@ -1,6 +1,9 @@
-import { Switch } from "@arbeidstilsynet/design-react";
-import { ColorItem, ColorPalette } from "@storybook/addon-docs/blocks";
+import { Dropdown, Switch } from "@arbeidstilsynet/design-react";
+import { Unstyled } from "@storybook/addon-docs/blocks";
 import { useEffect, useRef, useState } from "react";
+import { ToastContainer } from "react-toastify";
+import { ColorPalette } from "./ColorPalette";
+import classes from "./ColorsSwatches.module.css";
 import { getGroupedThemeColors } from "./getThemeColors";
 
 export function ColorsSwatches() {
@@ -8,38 +11,76 @@ export function ColorsSwatches() {
   const [scheme, setScheme] = useState("light");
   const [colors, setColors] = useState<
     ReturnType<typeof getGroupedThemeColors>
-  >({});
+  >({ special: {}, ordinary: {} });
+  const [dataColor, setDataColor] = useState("accent");
 
   useEffect(() => {
     if (!parentRef.current) return;
     const themeColors = getGroupedThemeColors(parentRef.current);
     setColors(themeColors);
-  }, [scheme]);
+  }, [scheme, dataColor]);
 
   return (
-    <div
-      ref={parentRef}
-      data-color-scheme={scheme}
-      style={{ padding: "1rem", background: "#ffffff" }}
-      className="colors-demo-scope"
-    >
+    <Unstyled>
       <div
-        data-color-scheme="light"
-        style={{ display: "flex", gap: "0.5rem", marginBottom: "1rem" }}
+        ref={parentRef}
+        data-color-scheme={scheme}
+        className={classes.container}
+        data-color={dataColor}
       >
-        <Switch
-          checked={scheme === "dark"}
-          label="Dark color scheme"
-          onChange={() => {
-            setScheme(scheme === "light" ? "dark" : "light");
-          }}
-        />
+        <div
+          data-color-scheme="light"
+          data-color={dataColor}
+          className={classes.inputs}
+        >
+          <Switch
+            checked={scheme === "dark"}
+            label="Dark color scheme"
+            onChange={() => {
+              setScheme(scheme === "light" ? "dark" : "light");
+            }}
+          />
+          <Dropdown.TriggerContext>
+            <Dropdown.Trigger className={classes.triggerButton} data-size="sm">
+              [data-color="{dataColor}"]
+            </Dropdown.Trigger>
+            <Dropdown
+              placement="bottom-end"
+              className={classes.dropdownContent}
+            >
+              <Dropdown.List>
+                {Object.keys(colors.ordinary).map((color) => (
+                  <Dropdown.Item key={color}>
+                    <Dropdown.Button
+                      data-color={color.toLowerCase()}
+                      value={color.toLowerCase()}
+                      onClick={() => setDataColor(color.toLowerCase())}
+                      className={classes.listItemButton}
+                    >
+                      {color}
+                    </Dropdown.Button>
+                  </Dropdown.Item>
+                ))}
+              </Dropdown.List>
+            </Dropdown>
+          </Dropdown.TriggerContext>
+        </div>
+        <div className={classes.infoText}>
+          <span>Semantic colors: </span>
+          <span>
+            Changes based on the closest ancestor with a "data-color" attribute
+            (Defaults to Accent)
+          </span>
+        </div>
+        <ColorPalette palette={colors.special} />
+        <br />
+        <div className={classes.infoText}>
+          <span>Color palette: </span>
+          <span>Only changes with the dark color scheme</span>
+        </div>
+        <ColorPalette palette={colors.ordinary} />
       </div>
-      <ColorPalette>
-        {Object.entries(colors).map(([name, value]) => (
-          <ColorItem key={name} title={name} colors={value} subtitle="" />
-        ))}
-      </ColorPalette>
-    </div>
+      <ToastContainer position="bottom-center" autoClose={3000} pauseOnHover />
+    </Unstyled>
   );
 }
