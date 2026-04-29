ea04169ac95be13f4c091b80fba6a6899f6d6662
Grunde Thorheim <grunde.thorheim@arbeidstilsynet.no>
2026-04-29 10:06:07 +0200

    add NOSONAR comments to ESLint stubs and update disable directive

--- tools/eslint-stub/use-at-your-own-risk.cjs
+++ tools/eslint-stub/use-at-your-own-risk.cjs
@@ -1,4 +1,6 @@
+// oxlint-disable typescript/no-extraneous-class
+
 // Stub for eslint/use-at-your-own-risk, used by @typescript-eslint/utils FlatESLint.js
-class FlatESLint {}
+class FlatESLint {} // NOSONAR
 
 module.exports = { FlatESLint };
